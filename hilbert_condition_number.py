5d6caafdd98d66c3d0bb86300fd4b9fcdcb840dc
David Martin Vela <david.mv@nuvolar.eu>
2021-03-08 16:11:09 +0100

    Remove unnecesary calculations

--- hilbert_condition_number.py
+++ hilbert_condition_number.py
@@ -2,31 +2,15 @@ from scipy.linalg import hilbert
 import numpy as np
 from numpy import linalg as LA
 
-for n in range(2,5):        
+for n in range(2,6):        
     # Create a Hilbert matrix of order n
     h = hilbert(n)
     # Calculate inverse
     h_inv = LA.inv(h)
     print("Matriz de hilbert de orden: " + str(n) +"\n", h)
-    print("Matriz inversa de hilbert de orden: " + str(n) +"\n", h_inv)
-    # Calculate norm 1
-    norm_1 = LA.norm(h, 1)
-    norm_1_inv = LA.norm(h_inv, 1)
-    print("Norma 1: ", norm_1)
-    print("Norma 1 de la inversa: ", norm_1_inv)
     # Calculate condition number using norm 1
-    print("Condicionamiento de la matriz en norma 1: ", norm_1*norm_1_inv)
-    # Calculate norm 2
-    norm_2 = LA.norm(h, 2)
-    norm_2_inv = LA.norm(h_inv, 2)
-    print("Norma 2: ", norm_2)
-    print("Norma 2 de la inversa: ", norm_2_inv)
+    print("Condicionamiento de la matriz en norma 1: ", LA.cond(h, 1))
     # Calculate condition number using norm 2
-    print("Condicionamiento de la matriz en norma 2: ", norm_2*norm_2_inv)
-    # Calculate norm infinite
-    norm_inf = LA.norm(h, np.inf)
-    norm_inf_inv = LA.norm(h_inv, np.inf)
-    print("Norma infinito: ", norm_inf)
-    print("Norma infinito de la inversa: ", norm_inf_inv)
+    print("Condicionamiento de la matriz en norma 2: ", LA.cond(h, 2))
     # Calculate condition number using norm infinite
-    print("Condicionamiento de la matriz en norma infinito: ", norm_inf*norm_inf_inv)
+    print("Condicionamiento de la matriz en norma infinite: ", LA.cond(h, np.inf))
